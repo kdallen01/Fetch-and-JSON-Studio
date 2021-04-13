595c3a33c7afe9f8619c636de228977ed6d62b0c
unknown <kdallen@accessus.net>
2021-04-13 10:37:51 -0500

    Completed excluding bonus

--- script.js
+++ script.js
@@ -5,23 +5,30 @@ window.addEventListener('load', function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
             const container = document.getElementById("container");
-            let index = 0
             for(let index = 0; index < json.length; index++) {
-                container.innerHTML = `
+                let astronaut = document.createElement("div");
+                astronaut.innerHTML = `
                     <div class="astronaut">
                         <div class="bio">
                         <h3>${json[index].firstName} ${json[index].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${json[index].hoursInSpace}</li>
-                            <li>Active: ${json[index].active}</li>
+                            <li id="Active">Active: ${json[index].active}</li>
                             <li>Skills: ${json[index].skills}</li>
                         </ul>
                         </div>
                         <img class="avatar" src=${json[index].picture}></img>
                     </div>
                 `;
+                document.body.appendChild(astronaut);
             }
-                index = (index++) % json.length
+            //Start to Number of Astronauts - Do Not Delete
+            const sum = document.createElement("div");
+            sum.innerHTML = `
+            <h3>Number of Astronauts: ${json.length}</h3>
+            `;
+            document.body.appendChild(sum);
+            //End of number of Astronauts
             });
         });
     });
